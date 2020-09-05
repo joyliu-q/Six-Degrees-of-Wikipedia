39ba3279f18e2e1b6ca59fe3dc73717a2308143c
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-09-04 23:44:33 -0700

    recursion + path tracking

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ from urllib.request import urlopen
 
 # Dismissed links are links shared by all sites and do not factor into 6 degrees of separation
 dismissed_links = ["Talk", "Categories", "Contributions", "Article", "Read", "Main page", "Contents", "Current events", "Random article", "About Wikipedia", "Help", "Community portal", "Recent changes", "Upload file", "What links here", "Related changes", "Upload file", "Special pages", "About Wikipedia", "Disclaimers", "Articles with short description", "Short description matches Wikidata", "Wikipedia indefinitely semi-protected biographies of living people", "Use mdy dates from October 2016", "Articles with hCards", "BLP articles lacking sources from October 2017", "All BLP articles lacking sources", "Commons category link from Wikidata", "Articles with IBDb links", "Internet Off-Broadway Database person ID same as Wikidata", "Short description is different from Wikidata", "PMID", "ISBN", "doi"] 
-degree = 1
+degree = 0
+path = []
+
+class node:
+    def __init__(self, title, url, prev, next):
+        self.title = title
+        self.url = url
+        self.prev = prev
+        self.next = next
 
 # find_connections - A function that returns all relevant referrals by Wikipedia
 def return_connections(url):
@@ -28,34 +36,48 @@ def return_connections(url):
     return relevant_entries
 
 # Supplementl function for determine_degrees()
-def find_common(entries1, entries2):
-    url1_keys = set(entries1.keys())
-    url2_keys = set(entries2.keys())
+def find_common(url1, url2):
+    connections_1 = return_connections(url1)
+    connections_2 = return_connections(url2)
+
+    url1_keys = set(connections_1.keys())
+    url2_keys = set(connections_2.keys())
     intersection = {}
     for key in url1_keys.intersection(url2_keys): 
-        intersection[key] = entries1[key]
+        intersection[key] = connections_1[key]
     return intersection
 
 # Determine amount of degrees and commonalities, with origin and end being urls
-def determine_degrees(origin, end):
+def determine_degrees(origin_url, end_url):
     # Check degree
     global degree
-    origin_connections = return_connections(origin)
-    end_connections = return_connections(end)
-    shared_connections = find_common(origin_connections,end_connections)
+    global track
 
-    # Print
-    print("Python so slow go brrr")
+    degree += 1
+
+    origin_connections = return_connections(origin_url)
+    end_connections = return_connections(end_url)
+    shared_connections = find_common(origin_url,end_url)
     
     # If there are shared connections, return
     if len(shared_connections) != 0:
         return [degree, shared_connections]
     # Otherwise, raise degree and keep searching
-    degree += 1
-    for entry_o in origin_connections.values():
+    else: 
+        degree += 1
+        for entry_o in origin_connections.values():
+            shared_connections = find_common("https://en.wikipedia.org" + entry_o, end_url)
+            if len(shared_connections) != 0:
+                return [degree, shared_connections]
         for entry_e in end_connections.values():
+            shared_connections = find_common("https://en.wikipedia.org" + entry_e, origin_url)
+            if len(shared_connections) != 0:
+                return [degree, shared_connections]
+        else:
+            print("bruh")
             determine_degrees("https://en.wikipedia.org" + entry_o, "https://en.wikipedia.org" + entry_e)
+        
 
-degree_info = determine_degrees("https://en.wikipedia.org/wiki/Six_Degrees_of_Kevin_Bacon", "https://en.wikipedia.org/wiki/Mao")
+degree_info = determine_degrees("https://en.wikipedia.org/wiki/Six_Degrees_of_Kevin_Bacon", "https://en.wikipedia.org/wiki/Satan")
 
-print(str(degree_info[0]) + "\n" + str(degree_info[1]))
+print("Degree: " + str(degree_info[0]) + "\nShared Connections: " + str(degree_info[1]))
