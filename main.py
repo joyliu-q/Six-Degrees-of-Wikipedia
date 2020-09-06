976b80b3d1bc1dbd2c2fc69b2046cd971fc0d041
Qijia "Joy" Liu <joyliu.q@gmail.com>
2020-09-06 15:33:46 -0700

    organization

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from concurrent.futures import Future
 
 # Dismissed links are links shared by all sites and do not factor into 6 degrees of separation
 dismissed_links = ["Talk", "Categories", "Contributions", "Article", "Read", "Main page", "Contents", "Current events", "Random article", "About Wikipedia", "Help", "Community portal", "Recent changes", "Upload file", "What links here", "Related changes", "Upload file", "Special pages", "About Wikipedia", "Disclaimers", "Articles with short description", "Short description matches Wikidata", "Wikipedia indefinitely semi-protected biographies of living people", "Use mdy dates from October 2016", "Articles with hCards", "BLP articles lacking sources from October 2017", "All BLP articles lacking sources", "Commons category link from Wikidata", "Articles with IBDb links", "Internet Off-Broadway Database person ID same as Wikidata", "Short description is different from Wikidata", "PMID", "ISBN", "doi"] 
-degree = 1
+degree = 0
 path = []
 path_found = False
 current_generation = []
@@ -75,15 +75,15 @@ def attempt_match_children(current_node, to_node):
     # print(str(current_node.children))
     # Check children for any matches w/ to_node 
     for child_node in current_node.children:
-        # Mark child_node as already searched
-        child_node.searched = True
-        # If child_node is the to_node, search is over
-        if child_node.url == to_node.url:
-            path.append(current_node)
-            path.append(child_node)
-            print("yaw")
-            path_found = True
-            return True
+        # Check if child_node was already searched
+        if child_node.searched == False:
+            child_node.searched = True
+            # If child_node is the to_node, search is over
+            if child_node.url == to_node.url:
+                path.append(current_node)
+                path.append(child_node)
+                path_found = True
+                return True
     return False
 
 # Determine amount of degrees and commonalities, with origin and end being urls
@@ -91,6 +91,7 @@ def determine_path(from_node, to_node):
     global path_found
     global current_generation
     global path
+    global degree
 
     # 0th degree
     if from_node.url == to_node.url:
@@ -98,8 +99,8 @@ def determine_path(from_node, to_node):
         return path
 
     # 1st degree
+    degree += 1
     attempt_match_children(from_node, to_node)
-    path.append(from_node)
 
     # 2+ degree: if none of the children match, continue to search through loop
     current_node = from_node
@@ -107,66 +108,35 @@ def determine_path(from_node, to_node):
     temp_generation = []
     
     while path_found == False: 
-        print("degree xd")
+        degree += 1
+        print(len(current_generation))
         # Keep Looping through each sibling_node and check sibling's children
         for sibling_node in current_generation:
             attempt_match_children(sibling_node, to_node)
             temp_generation.extend(sibling_node.children)
-            # If found match in 2nd degree
+            # If found match in current degree
+            print(len(temp_generation))
             if path_found == True:
+                print("yas")
                 return sibling_node
         # If none of the siblings in the level matched, move to higher degree
         if path_found == False:
             current_generation = temp_generation
+            temp_generation = []
 
     return path
-    
-    
-    """# Check degree
-    global degree
-    global path
-
-    path_found = False
-
-    path.append(from_node)
-    from_node.find_children()
-
-    # Check if any child_node is the to_node
-    if to_node in from_node.children:
-        path.append(to_node)
-        path_found = True
-        return
-         # If not, raise degree
-    
-    current_node = from_node
-    while path_found == False:
-        start = time.time()
-        # Use ThreadPool to find all children data
-        with ThreadPoolExecutor(max_workers=None) as executor:
-            futures = [executor.submit(child_node.find_children) for child_node in current_node.children]
-            results = []
-            for f in concurrent.futures.as_completed(futures):
-                results.append(f.result())
-                number += 1
-        # Check children for any matches w/ 
-        for child_node in current_node.children:
-            child_node.searched = True
-            if child_node.url == to_node.url:
-                path.append(child_node)
-                path_found = True
-                return
-        print(time.time() - start)
-    """
 
 
 def main():
     global current_generation
     root = Node("Kevin Bacon", "https://en.wikipedia.org/wiki/Kevin_Bacon")
     current_generation.append(root)
-    target = Node("Law of attraction (New Thought)", "https://en.wikipedia.org/wiki/Law_of_attraction_(New_Thought)")
+    target = Node("Elizabeth Perkins", "https://en.wikipedia.org/wiki/Elizabeth_Perkins")
     determine_path(root, target)
+    print("Path:")
     for node in path:
         print(node.url)
+    print("Degree: " + str(degree))
 
 if __name__ == '__main__':
     main()
